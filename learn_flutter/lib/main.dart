554f854c5af32a21df198f8bf0ffffd31825bff9
lowqilong <lowqilong@hotmail.com>
2020-12-04 04:02:26 +0800

    abit of progress

--- learn_flutter/lib/main.dart
+++ learn_flutter/lib/main.dart
@@ -5,6 +5,22 @@ void main() => runApp(MyCoolApp());
 class MyCoolApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'));
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('My First App'),
+      ),
+      body: Column(children: [
+        Text('The question!'),
+        RaisedButton(
+          child: Text('Answer 1'),
+          onPressed: null,
+        ),
+      ]),
+    ));
   }
 }
